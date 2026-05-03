3f684499664fa97427abff965c7307cc9f7258ed
Kasun Sagara <kasunsagara689@gmail.com>
2026-05-03 14:18:52 +0530

    create auth module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+})
 export class UsersModule {}
