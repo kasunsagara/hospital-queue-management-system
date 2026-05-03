fec2bfed31134feab9a15ef9bdd3927a644f527a
Kasun Sagara <kasunsagara689@gmail.com>
2026-05-03 13:49:25 +0530

    install required packages and create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { QueueModule } from './queue/queue.module';
+import { ServicesModule } from './services/services.module';
+import { CountersModule } from './counters/counters.module';
+import { WebsocketModule } from './websocket/websocket.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, QueueModule, ServicesModule, CountersModule, WebsocketModule],
   controllers: [AppController],
   providers: [AppService],
 })
